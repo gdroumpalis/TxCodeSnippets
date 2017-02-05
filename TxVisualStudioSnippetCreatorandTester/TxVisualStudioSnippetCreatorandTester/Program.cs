77118791ac911c393dc7bc88e1efc7d6e5b10fc8
George Droubalis <georgiosdroubalis@hotmail.gr>
2017-02-05 19:53:34 +0200

    move all snippets in snippets folder for easiest path

--- TxVisualStudioSnippetCreatorandTester/TxVisualStudioSnippetCreatorandTester/Program.cs
+++ TxVisualStudioSnippetCreatorandTester/TxVisualStudioSnippetCreatorandTester/Program.cs
@@ -4,30 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace TxVisualStudioSnippetCreatorandTester
 {
 
     class Program
     {
 
-
+        static readonly string source = "";
+        static readonly string dest = "";
         static void Main(string[] args)
         {
-            Console.WriteLine(methodname(5,6)); 
-            Console.ReadKey();
+
         }
-        public static string Test()
-        {
-            return "tetststs";
-        }
-        public static string methodname(int x, int y)
-        {
-            return $"{x}+{y}";
-        }
+    
+
 
     }
 
-    
+
 
 
 }
