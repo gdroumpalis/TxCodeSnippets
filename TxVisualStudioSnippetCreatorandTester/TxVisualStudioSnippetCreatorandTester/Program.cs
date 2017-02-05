c6f81f1fffa9fdc607525b16e09a93728c890f4c
George Droubalis <georgiosdroubalis@hotmail.gr>
2017-02-05 20:05:35 +0200

    Move surround outside of snippets folder

--- TxVisualStudioSnippetCreatorandTester/TxVisualStudioSnippetCreatorandTester/Program.cs
+++ TxVisualStudioSnippetCreatorandTester/TxVisualStudioSnippetCreatorandTester/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,18 +12,56 @@ namespace TxVisualStudioSnippetCreatorandTester
     class Program
     {
 
-        static readonly string source = "";
-        static readonly string dest = "";
+        static readonly string source = @"C:\Users\TerminuX\Desktop\TxCodeSnippets\TxVisualStudioSnippetCreatorandTester\TxVisualStudioSnippetCreatorandTester\Snippets";
+        static readonly string dest = @"C:\Users\TerminuX\Documents\Visual Studio 2015\Code Snippets\Visual C#\My Code Snippets";
         static void Main(string[] args)
         {
+            DirectoryCopy(source, dest, true);
+
+            Console.ReadKey();
+        }
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
 
         }
-    
-
 
     }
-
-
-
-
 }
+
+
+
